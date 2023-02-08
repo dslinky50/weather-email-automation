5d17206c8b5755df0fefc1da0dfc54ce8d62f516
Dylan Silinski <dylan.silinski@gmail.com>
2023-02-07 19:28:00 -0500

    syntax

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,4 +1,4 @@
-resource "aws_lambda_function" "email-automation" {
+resource "aws_lambda_function" "email_automation" {
     function_name = "weather-email-automation"
     filename = "./lambda/lambda.zip"
     handler = "weather_email.lambda_handler"
