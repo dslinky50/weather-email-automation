3728f0d7dc2a5c3ef4a8cc86ea8e840f8050c851
Dylan Silinski <dylan.silinski@gmail.com>
2023-02-07 19:24:47 -0500

    first deployment try

---
+++ terraform/lambda.tf
@@ -0,0 +1,8 @@
+resource "aws_lambda_function" "email-automation" {
+    function_name = "weather-email-automation"
+    filename = "./lambda/lambda.zip"
+    handler = "weather_email.lambda_handler"
+    role = aws_iam_role.email_automation_lambda_role.arn
+    runtime = "python3.9"
+    timeout = "5"
+}
