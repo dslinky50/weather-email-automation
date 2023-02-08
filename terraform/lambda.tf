96c770652a281b4ab44495c8816fd0875c4c98be
Dylan Silinski <dylan.silinski@gmail.com>
2023-02-08 12:00:36 -0500

    try something simple

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,21 +1,21 @@
-data "archive_file" "email_automation_func" {
-  type        = "zip"
-  source_file = "./lambda/weather_email.py"
-  output_path = "./lambda/lambda.zip"
-}
+# data "archive_file" "email_automation_func" {
+#   type        = "zip"
+#   source_file = "./lambda/weather_email.py"
+#   output_path = "./lambda/lambda.zip"
+# }
 
-data "archive_file" "email_automation_layer" {
-  type        = "zip"
-  source_file = "./lambda/requirements.zip"
-  output_path = "./lambda/requirements.zip"
-}
+# data "archive_file" "email_automation_layer" {
+#   type        = "zip"
+#   source_file = "./lambda/requirements.zip"
+#   output_path = "./lambda/requirements.zip"
+# }
 
-resource "aws_lambda_layer_version" "email_automation_layer" {
-  filename   = "./lambda/requirements.zip"
-  layer_name = "weather-email-automation-layer"
-  source_code_hash = data.archive_file.email_automation_layer.output_base64sha256
-  compatible_runtimes = ["python3.9"]
-}
+# resource "aws_lambda_layer_version" "email_automation_layer" {
+#   filename   = "./lambda/requirements.zip"
+#   layer_name = "weather-email-automation-layer"
+#   source_code_hash = data.archive_file.email_automation_layer.output_base64sha256
+#   compatible_runtimes = ["python3.9"]
+# }
 
 resource "aws_lambda_function" "email_automation" {
     environment {
@@ -30,10 +30,10 @@ resource "aws_lambda_function" "email_automation" {
     function_name = "weather-email-automation"
     filename = "./lambda/lambda.zip"
     handler = "weather_email.lambda_handler"
-    layers = [ aws_lambda_layer_version.email_automation_layer.arn ]
+    # layers = [ aws_lambda_layer_version.email_automation_layer.arn ]
     role = aws_iam_role.email_automation_lambda_role.arn
     runtime = "python3.9"
-    source_code_hash = data.archive_file.email_automation_func.output_base64sha256
+    source_code_hash = filebase64sha256("./lambda/lambda.zip")
     timeout = "5"
 
     tags = {
