bdbadf09d2de3355f3f0d871d85e61fb31bde529
Dylan Silinski <dylan.silinski@gmail.com>
2023-02-08 08:59:34 -0500

    lambda env vars

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,4 +1,13 @@
 resource "aws_lambda_function" "email_automation" {
+    environment {
+      variables = {
+        CLIENT_ID = "${var.CLIENT_ID}",
+        CLIENT_SECRET = "${var.CLIENT_SECRET}",
+        EMAIL_LIST = "${var.EMAIL_LIST}",
+        REFRESH_TOKEN = "${var.REFRESH_TOKEN}",
+        WEATHER_API_KEY = "${var.WEATHER_API_KEY}",
+      }
+    }
     function_name = "weather-email-automation"
     filename = "./lambda/lambda.zip"
     handler = "weather_email.lambda_handler"
@@ -9,5 +18,5 @@ resource "aws_lambda_function" "email_automation" {
     tags = {
       "Name" = "Bandon"
     }
-    
+
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "CLIENT_ID" {
+  type = string
+}
+
+variable "CLIENT_SECRET" {
+  type = string
+}
+
+variable "EMAIL_LIST" {
+  type = string
+}
+
+variable "REFRESH_TOKEN" {
+  type = string
+}
+
+variable "WEATHER_API_KEY" {
+  type = string
+}
