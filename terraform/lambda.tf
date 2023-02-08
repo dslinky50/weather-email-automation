79913e955eecbe111146068ec510c56b94988a74
Dylan Silinski <dylan.silinski@gmail.com>
2023-02-08 09:51:35 -0500

    try a lmabda layer

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,20 @@
-data "archive_file" "email_automation" {
+data "archive_file" "email_automation_func" {
   type        = "zip"
   source_file = "./lambda/weather_email.py"
   output_path = "./lambda/lambda.zip"
+}
+
+data "archive_file" "email_automation_layer" {
+  type        = "zip"
+  source_file = "./lambda/requirements.zip"
+  output_path = "./lambda/requirements.zip"
+}
+
+resource "aws_lambda_layer_version" "lambda_layer" {
+  filename   = "./lambda/requirements.zip"
+  layer_name = "weather-email-automation-layer"
+  source_code_hash = data.archive_file.email_automation_layer.output_base64sha256
+  compatible_runtimes = ["python3.9"]
 }
 
 resource "aws_lambda_function" "email_automation" {
@@ -17,9 +30,10 @@ resource "aws_lambda_function" "email_automation" {
     function_name = "weather-email-automation"
     filename = "./lambda/lambda.zip"
     handler = "weather_email.lambda_handler"
+    layers = [ "weather-email-automation-layer" ]
     role = aws_iam_role.email_automation_lambda_role.arn
     runtime = "python3.9"
-    source_code_hash = data.archive_file.email_automation.output_base64sha256
+    source_code_hash = data.archive_file.email_automation_func.output_base64sha256
     timeout = "5"
 
     tags = {
