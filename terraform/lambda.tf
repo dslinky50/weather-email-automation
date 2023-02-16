cd53947cbcb7c747889c7e168092d097f805d8a6
Dylan Silinski <dylan.silinski@gmail.com>
2023-02-16 15:43:55 -0500

    whoops

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -15,7 +15,7 @@ resource "aws_lambda_function" "email_automation" {
     function_name = "weather-email-automation"
     filename = "./lambda/lambda.zip"
     handler = "weather_email.lambda_handler"
-    layers = [aws_lambda_layer_version.email_automation_layer.arn]
+    # layers = [aws_lambda_layer_version.email_automation_layer.arn]
     role = aws_iam_role.email_automation_lambda_role.arn
     runtime = "python3.9"
     source_code_hash = filebase64sha256("./lambda/lambda.zip")
