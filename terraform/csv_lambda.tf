56a6938418307b1d11dff9cb7775d1f59cd6deec
Dylan Silinski <dylan.silinski@gmail.com>
2023-05-05 17:28:06 -0400

    update role

--- terraform/csv_lambda.tf
+++ terraform/csv_lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "csv_automation" {
     filename = "./lambda/dynamo_s3.zip"
     handler = "dynamo_s3.lambda_handler"
     # layers = [aws_lambda_layer_version.email_automation_layer.arn]
-    role = aws_iam_role.email_automation_lambda_role.arn
+    role = aws_iam_role.csv_automation_lambda_role.arn
     runtime = "python3.9"
     source_code_hash = filebase64sha256("./lambda/dynamo_s3.zip")
     timeout = "10"
